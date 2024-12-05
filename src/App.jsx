2494812ce0c7e27ec0cf8ef512e5e5f1e7e9a41b
Mohammadreza <ma3968312@gmail.com>
2024-12-05 18:58:50 +0330

    routing in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckOutPage from "./pages/CheckOutPage";
+import PageNotFound from "./pages/404";
+
 function App() {
   return (
-    <>
-      <h1>Store</h1>{" "}
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 
