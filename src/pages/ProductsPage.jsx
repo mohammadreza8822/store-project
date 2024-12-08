bd969482fb53cf9ba2f9bda9cdb98eba2df639f8
Mohammadreza <ma3968312@gmail.com>
2024-12-08 10:37:40 +0330

    add loader and category

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,63 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 
 import styles from "./ProductsPage.module.css";
+import { TbPentagram } from "react-icons/tb";
 
 function ProductsPage() {
   const products = useProducts();
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {};
+
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLocaleLowerCase();
+
+    if (tagName !== "LI") return;
+    console.log(category);
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <p>Loading...</p>}
-        {products.map((p) => (
-          <Card key={p.id} data={p}/>
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
