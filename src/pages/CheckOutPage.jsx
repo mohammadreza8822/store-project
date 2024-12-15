c6c50ad4a411c15f8f1c800f00dd89dfa62e840e
Mohammadreza <ma3968312@gmail.com>
2024-12-15 19:37:27 +0330

    implement basketcard

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,5 +1,37 @@
+import BasketCard from "../components/BasketCard";
+import BasketSideBar from "../components/BasketSideBar";
+import { useCart } from "../context/CartContext";
+
+import styles from "./CheckOutPage.module.css";
+
 function CheckOutPage() {
-  return <div>CheckOutPage</div>;
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => {
+    dispatch({ type, payload });
+  };
+
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.container}>
+      <BasketSideBar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckOutPage;
