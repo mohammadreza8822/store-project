2f9cc06b7cb7e50426a8fe10a091c9554cb6a283
Mohammadreza <ma3968312@gmail.com>
2024-12-15 17:20:50 +0330

    create detail component

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -31,5 +31,11 @@ const useProducts = () => {
   return products;
 }; // custom hook
 
+const useProductDetails = (id) => {
+  const products = useContext(ProductContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
+
 export default ProductProvider;
-export { useProducts };
+export { useProducts, useProductDetails };
